df10a5545fb7717337d4890894c5643460105b21
dimateus84 <stoppasivcom@yandex.ua>
2015-12-23 16:19:27 +0300

    adaprtivnaya 2 gallery

--- 13-0297-WC-Will-Rogers-Gallery.php
+++ 13-0297-WC-Will-Rogers-Gallery.php
@@ -1,6 +1,6 @@
 <?php $title = '13-0297-WC-Will-Rogers-Gallery'; ?>
 <?php include 'tpl/includes/head.inc'; ?>
-<body class="page page-gallery">
+<body class="page page-gallery" xmlns="http://www.w3.org/1999/html">
 <div class="outer-wrapper">
   <?php include 'tpl/layout/header.inc'; ?>
   <div class="inner-wrapper site-container">
@@ -10,26 +10,47 @@
       <div class="content-bottom">
         <div class="text-img-wrapper">
           <div class="gallery-btn-wrapper">
-          <div class="btn-wrapper">
-            <a class="btn active" href="#">View All</a>
+            <div class="btn-wrapper">
+              <a class="btn active" href="#">View All</a>
+            </div>
+            <div class="btn-wrapper">
+              <a class="btn" href="#">Weddings</a>
+            </div>
+            <div class="btn-wrapper">
+              <a class="btn" href="#">Business</a>
+            </div>
+            <div class="btn-wrapper">
+              <a class="btn" href="#">Party</a>
+            </div>
+            <div class="btn-wrapper">
+              <a class="btn" href="#">Presentation</a>
+            </div>
           </div>
-          <div class="btn-wrapper">
-            <a class="btn" href="#">Weddings</a>
+
+          <div class="form form-gallery-event">
+            <form action="#" method="post">
+              <div class="field-select-gallery field-item">
+                <div class="form-item form-type-select">
+                  <label>Select gallery:</label>
+
+                  <div class="select">
+                    <select class="form-select">
+                      <option>View All</option>
+                      <option>Weddings</option>
+                      <option>Business</option>
+                      <option>Party</option>
+                      <option>Presentation</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="btn-wrapper">
-            <a class="btn" href="#">Business</a>
           </div>
-          <div class="btn-wrapper">
-            <a class="btn" href="#">Party</a>
-          </div>
-          <div class="btn-wrapper">
-            <a class="btn" href="#">Presentation</a>
-          </div>
-          </div>
-        </div>
 
         <div class="b-gallery-event style-a">
           <h3><span>Event one</span></h3>
+
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas
             sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, consectetur ac, vestibulum at eros.
             Cras mattis consectetur purus sit amet fermentum.
@@ -47,6 +68,7 @@
 
         <div class="b-gallery-event style-a">
           <h3><span>Event Two</span></h3>
+
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas
             sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, consectetur ac, vestibulum at eros.
             Cras mattis consectetur purus sit amet fermentum.
@@ -63,6 +85,7 @@
 
         <div class="b-gallery-event style-a">
           <h3><span>Event Three</span></h3>
+
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas
             sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, consectetur ac, vestibulum at eros.
             Cras mattis consectetur purus sit amet fermentum.
