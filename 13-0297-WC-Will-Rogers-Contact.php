a83348f3c7f9ba67591aa7c62097108785fc5856
dimateus84 <stoppasivcom@yandex.ua>
2016-01-04 19:19:26 +0300

    pravka contact forms 2

--- 13-0297-WC-Will-Rogers-Contact.php
+++ 13-0297-WC-Will-Rogers-Contact.php
@@ -30,150 +30,156 @@
 
 <div class="b-cols col-one style-a">
 
-  <div class="col col-c col-1">
-    <div class="col-item">
+<div class="col col-c col-1">
+  <div class="col-item">
 
-      <h3><span>Contact us</span></h3>
+    <h3><span>Contact us</span></h3>
 
-      <div class="col-wrapper">
+    <div class="col-wrapper">
 
-        <div>
-          <p>All the information requested below is to help provide you with the right package, we understand
-            each event is special and we will sti down with you to discuss the best options we can offer to make
-            your event memorable. Thank You.</p>
-        </div>
+      <div>
+        <p>All the information requested below is to help provide you with the right package, we understand
+          each event is special and we will sti down with you to discuss the best options we can offer to make
+          your event memorable. Thank You.</p>
+      </div>
 
-        <div class="form form-contact">
-          <form action="#" method="post">
+      <div class="form form-contact">
+        <form action="#" method="post">
 
-            <div class="field-first-name field-item">
-              <div class="form-item form-type-text">
-                <label>First Name <span class="form-required">*</span></label>
-                <input type="text" class="form-text"/>
+          <div class="field-first-name field-item">
+            <div class="form-item form-type-text">
+              <label>First Name <span class="form-required">*</span></label>
+              <input type="text" class="form-text"/>
+            </div>
+          </div>
+
+          <div class="field-last-name field-item">
+            <div class="form-item form-type-text">
+              <label>Last Name <span class="form-required">*</span></label>
+              <input type="text" class="form-text required"/>
+            </div>
+          </div>
+
+          <div class="field-your-email-address field-item">
+            <div class="form-item form-type-email">
+              <label>Your Email Address <span class="form-required">*</span></label>
+              <input type="email" class="form-text required"/>
+            </div>
+          </div>
+
+          <div class="field-your-phone-number field-item">
+            <div class="form-item form-type-text">
+              <label>Your Phone Number <span class="form-required">*</span></label>
+              <input type="text" class="form-text required"/>
+            </div>
+          </div>
+
+          <div class="field-type-of-event field-item">
+            <div class="form-item form-type-select">
+              <label>Type of Event:</label>
+
+              <div class="select">
+                <select class="form-select">
+                  <option>Business Meeting or Presentation</option>
+                </select>
               </div>
             </div>
+          </div>
 
-            <div class="field-last-name field-item">
-              <div class="form-item form-type-text">
-                <label>Last Name <span class="form-required">*</span></label>
-                <input type="text" class="form-text required"/>
+          <div class="field-preferred-month field-item">
+            <div class="form-item form-type-select">
+              <label>Preferred Date:</label>
+
+              <div class="select">
+                <select class="form-select">
+                  <option>Month</option>
+                </select>
               </div>
             </div>
+          </div>
 
-            <div class="field-your-email-address field-item">
-              <div class="form-item form-type-email">
-                <label>Your Email Address <span class="form-required">*</span></label>
-                <input type="email" class="form-text required"/>
+          <div class="field-preferred-day field-item">
+            <div class="form-item form-type-select">
+              <label>Day</label>
+
+              <div class="select">
+                <select class="form-select">
+                  <option>Day</option>
+                </select>
               </div>
             </div>
+          </div>
 
-            <div class="field-your-phone-number field-item">
-              <div class="form-item form-type-text">
-                <label>Your Phone Number <span class="form-required">*</span></label>
-                <input type="text" class="form-text required"/>
+          <div class="field-preferred-year field-item">
+            <div class="form-item form-type-select">
+              <label>Year</label>
+
+              <div class="select">
+                <select class="form-select">
+                  <option>Year</option>
+                </select>
               </div>
             </div>
+          </div>
 
-            <div class="field-type-of-event field-item">
-              <div class="form-item form-type-select">
-                <label>Type of Event:</label>
+          <div class="field-date-flexible field-item">
+            <div class="form-item form-type-radio">
+              <label>Date Flexible?</label>
 
-                <div class="select">
-                  <select class="form-select">
-                    <option>Business Meeting or Presentation</option>
-                  </select>
-                </div>
+              <div class="radio">
+                <label class="option"><input type="radio" class="form-radio"/>Yes</label>
+              </div>
+              <div class="radio">
+                <label class="option"><input type="radio" class="form-radio"/>No</label>
               </div>
             </div>
+          </div>
 
-            <div class="field-preferred-month field-item">
-              <div class="form-item form-type-select">
-                <label>Preferred Date:</label>
+          <div class="field-entertainment field-item">
+            <div class="form-item form-type-select">
+              <label>Entertainment:</label>
 
-                <div class="select">
-                  <select class="form-select">
-                    <option>Month</option>
-                  </select>
-                </div>
+              <div class="select">
+                <select class="form-select">
+                  <option>DJ</option>
+                </select>
               </div>
             </div>
+          </div>
 
-            <div class="field-preferred-day field-item">
-              <div class="form-item form-type-select">
-                <label>Day</label>
-
-                <div class="select">
-                  <select class="form-select">
-                    <option>Day</option>
-                  </select>
-                </div>
-              </div>
+          <div class="field-other-entertainment field-item">
+            <div class="form-item form-type-text">
+              <label>Other Entertainment:</label>
+              <input type="text" class="form-text"/>
             </div>
+          </div>
 
-            <div class="field-preferred-year field-item">
-              <div class="form-item form-type-select">
-                <label>Year</label>
-
-                <div class="select">
-                  <select class="form-select">
-                    <option>Year</option>
-                  </select>
-                </div>
-              </div>
+          <div class="field-needs field-item">
+            <div class="form-item form-type-text">
+              <label>Audio and Visual Needs:</label>
+              <input type="text" class="form-text"/>
             </div>
+          </div>
 
-            <div class="field-date-flexible field-item">
-              <div class="form-item form-type-radio">
-                <label>Date Flexible?</label>
-
-                <div class="radio">
-                  <label class="option"><input type="radio" class="form-radio"/>Yes</label>
-                </div>
-                <div class="radio">
-                  <label class="option"><input type="radio" class="form-radio"/>No</label>
-                </div>
-              </div>
+          <div class="field-other-needs field-item">
+            <div class="form-item form-type-text">
+              <label>Other Needs:</label>
+              <input type="text" class="form-text"/>
             </div>
+          </div>
 
-            <div class="field-entertainment field-item">
-              <div class="form-item form-type-select">
-                <label>Entertainment:</label>
-
-                <div class="select">
-                  <select class="form-select">
-                    <option>DJ</option>
-                  </select>
-                </div>
-              </div>
+          <div class="field-number-of-guests field-item">
+            <div class="form-item form-type-text">
+              <label>Number of Guests:</label>
+              <input type="text" class="form-text"/>
             </div>
+          </div>
 
-            <div class="field-other-entertainment field-item">
-              <div class="form-item form-type-text">
-                <label>Other Entertainment:</label>
-                <input type="text" class="form-text"/>
-              </div>
-            </div>
+          <fieldset class="form-wrapper">
 
-            <div class="field-needs field-item">
-              <div class="form-item form-type-text">
-                <label>Audio and Visual Needs:</label>
-                <input type="text" class="form-text"/>
-              </div>
-            </div>
-
-            <div class="field-other-needs field-item">
-              <div class="form-item form-type-text">
-                <label>Other Needs:</label>
-                <input type="text" class="form-text"/>
-              </div>
-            </div>
-
-            <div class="field-number-of-guests field-item">
-              <div class="form-item form-type-text">
-                <label>Number of Guests:</label>
-                <input type="text" class="form-text"/>
-              </div>
-            </div>
+            <legend>
+              <span class="fieldset-legend">Budget:</span>
+            </legend>
 
             <div class="field-budget field-item">
               <div class="form-item form-type-text">
@@ -191,35 +197,37 @@
               </div>
             </div>
 
-            <div class="field-questions-or-concerns field-item">
-              <div class="form-item form-type-text">
-                <label>Questions or concerns:</label>
-                <textarea class="form-text-area"></textarea>
-              </div>
+          </fieldset>
+
+          <div class="field-questions-or-concerns field-item">
+            <div class="form-item form-type-text">
+              <label>Questions or concerns:</label>
+              <textarea class="form-text-area"></textarea>
+            </div>
+          </div>
+
+          <div class="field-captcha field-item">
+
+            <div class="captcha-img-container">
+              <img src="theme/images/tmp/captcha.jpg" alt="">
+              <a href="#">Generate new captcha</a>
             </div>
 
-            <div class="field-captcha field-item">
-
-              <div class="captcha-img-container">
-                <img src="theme/images/tmp/captcha.jpg" alt="">
-                <a href="#">Generate new captcha</a>
-              </div>
-
-              <div class="form-item form-type-text">
-                <label>Enter the characters shown in the image.<span class="form-required">*</span></label>
-                <input type="text" class="form-text required"/>
-              </div>
+            <div class="form-item form-type-text">
+              <label>Enter the characters shown in the image.<span class="form-required">*</span></label>
+              <input type="text" class="form-text required"/>
             </div>
-            <div class="btn-wrapper">
-              <input class="btn" id="edit-submit" type="submit" name="sub" value="Submit">
-            </div>
-          </form>
-        </div>
-
+          </div>
+          <div class="btn-wrapper">
+            <input class="btn" id="edit-submit" type="submit" name="sub" value="Submit">
+          </div>
+        </form>
       </div>
-    </div>
 
+    </div>
   </div>
+
+</div>
 
 </div>
 
